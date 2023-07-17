b3e1fc00be4002732de0fee8a8db0cd027ebc92b
Austin Papritz <austin@papritz.dev>
2023-07-17 11:41:30 -0700

    route for madlib result in place

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 public class HomeController : Controller
   {
+    [Route("/madlib")]
+    public ActionResult MadLibResults() {
+      return View();
+    }
+
     [Route("/")]
     public ActionResult Form() {
       return View();
